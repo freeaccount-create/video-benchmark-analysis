c38caf43aca36c4d945251b6bfc2a84eae931ea8
freeaccount-create <freeaccount-create@users.noreply.github.com>
2026-05-31 17:16:09 +0800

    Add source tree: dreamsim

---
+++ benchmarks/dreamsim/dataset/download_unfiltered_dataset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p ./dataset_unfiltered
+cd dataset_unfiltered
+
+mkdir -p ref
+mkdir -p distort
+
+# Download NIGHTS dataset
+
+wget http://data.csail.mit.edu/nights/nights_unfiltered/nights_raw.csv
+
+# store those in a list and loop through wget and unzip and rm
+for i in {0..99..25}
+do
+    wget https://data.csail.mit.edu/nights/nights_unfiltered/ref_${i}_$(($i+24)).zip
+    unzip -q ref_${i}_$(($i+24)).zip -d ref
+    rm ref_*.zip
+
+    wget https://data.csail.mit.edu/nights/nights_unfiltered/distort_${i}_$(($i+24)).zip
+    unzip -q distort_${i}_$(($i+24)).zip -d distort
+    rm distort_*.zip
+done
